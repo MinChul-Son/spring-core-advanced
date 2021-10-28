488cc5ef193c5d813b3d1e951130a21c6ed4fd82
MinChul-Son <smc5236@naver.com>
2021-10-28 17:18:26 +0900

    feat : TraceId 생성
    - 트랜잭션 id와 level을 멤버로

---
+++ advanced/src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,39 @@
+package hello.advanced.trace;
+
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
+public class TraceId {
+
+    private final String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    /**
+     * HTTP 트랜잭션의 깊이를 표현하기 위해 동일한 id로 레벨을 +1 해주는 메소드
+     * @return TraceId
+     */
+    public TraceId createNextId() {
+        this.level++;
+        return this;
+    }
+
+    public TraceId createPreviousId() {
+        this.level--;
+        return this;
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+}
