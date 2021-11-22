66b4edc0a2117a3075629850664770dd2be5d780
MinChul-Son <smc5236@naver.com>
2021-11-22 18:36:30 +0900

    test : ProxyTargetClass 옵션
    - setProxyTargetClass() 옵션에 true를 주면 인터페이스가 있어도 클래스 기반으로 CGLIB을 사용

--- proxy-start/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ proxy-start/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -49,5 +49,24 @@ public class ProxyFactoryTest {
         Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
         Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
     }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB 사용")
+    void proxyTargetClass() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.find();
+
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
 }
 
