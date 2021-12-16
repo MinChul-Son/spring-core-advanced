d11e18694701b04ba564d9b72c90a8ea949891bb
MinChul-Son <smc5236@naver.com>
2021-12-16 18:25:33 +0900

    test : 멀티 Advisor
    - Spring AOP는 하나의 프록시를 생성하고 필요시 그 안에 여러개의 Advisor를 추가해야한다.

---
+++ proxy-start/src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,50 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+public class MultiAdvisorTest {
+
+    @Test
+    @DisplayName("하나의 프록시, 멀티 어드바이저")
+    void multiAdvisorTest() {
+        // client -> proxy -> Advisor2 -> Advisor1 -> target
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+
+        ServiceInterface target = new ServiceImpl();
+
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+        proxyFactory1.addAdvisor(advisor2);
+        proxyFactory1.addAdvisor(advisor1);
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory1.getProxy();
+        //실행
+        proxy.save();
+    }
+
+    @Slf4j
+    static class Advice1 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice1 호출");
+            return invocation.proceed();
+        }
+    }
+    @Slf4j
+    static class Advice2 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice2 호출");
+            return invocation.proceed();
+        }
+    }
+}
