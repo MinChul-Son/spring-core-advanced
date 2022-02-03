ec7f0218cd62c88171f23425f950be99f90e92b7
MinChul-Son <smc5236@naver.com>
2022-02-03 21:50:05 +0900

    feat: AOP를 적용하기 위한 단순한 service, repository 생성

--- aop/src/main/java/hello/aop/AopApplication.java
+++ aop/src/main/java/hello/aop/AopApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AopApplication.class, args);
+    }
 
 }
